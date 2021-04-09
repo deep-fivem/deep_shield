266b77b60ab7129efe6323002cd26b54cef2ad1f
deep-fivem <60301671+deep-fivem@users.noreply.github.com>
2021-04-09 20:16:19 +0200

    Changed and added few things
    hey :)

--- code/client.lua
+++ code/client.lua
@@ -1,8 +1,6 @@
---shieldActive_two szaar
-
---local shieldActive_two = false
 local shieldActive = false
 local shieldEntity = nil
+local wait = false
 
 -- ANIM
 local animDict = "combat@gestures@gang@pistol_1h@beckon"
@@ -12,7 +10,6 @@ local prop = "prop_ballistic_shield"
 local pistol = GetHashKey("WEAPON_PISTOL")
 
 ESX              = nil
-local PlayerData = {}
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -21,126 +18,100 @@ Citizen.CreateThread(function()
 	end
 end)
 
-RegisterNetEvent('esx:playerLoaded')
-AddEventHandler('esx:playerLoaded', function(xPlayer)
-  PlayerData = xPlayer   
-end)
+RegisterCommand("ammo", function(source)
+    ESX.ShowNotification("pistol ammo: "..GetAmmoInPedWeapon(GetPlayerPed(source), GetHashKey("WEAPON_PISTOL") ""))
+end, false)
 
-RegisterNetEvent("shield:toggle")
-AddEventHandler("shield:toggle", function()
-    print('toggle')
-    if shieldActive or shieldActive_two then
-        DisableShield()
-    else
-        EnableShield()
-    end
-end)
-
-function EnableShield()
-    local ped = GetPlayerPed(-1)
-    local pedPos = GetEntityCoords(ped, false)
-    local ammo = GetAmmoInPedWeapon(ped, pistol)
-    print('ammo: '..ammo) --need test
-    if HasPedGotWeapon(ped, pistol, 0) or GetSelectedPedWeapon(ped) == pistol then
-        print('has pistol')
-        shieldActive = true
-
-        RequestAnimDict(animDict)
-        while not HasAnimDictLoaded(animDict) do
-            Citizen.Wait(100)
-        end
-
-        TaskPlayAnim(ped, animDict, animName, 8.0, -8.0, -1, (2 + 16 + 32), 0.0, 0, 0, 0)
-
-        RequestModel(GetHashKey(prop))
-        while not HasModelLoaded(GetHashKey(prop)) do
-            Citizen.Wait(100)
-        end
-
-        local shield = CreateObject(GetHashKey(prop), pedPos.x, pedPos.y, pedPos.z, 1, 1, 1)
-        shieldEntity = shield
-
-        AttachEntityToEntity(shieldEntity, ped, GetEntityBoneIndexByName(ped, "IK_L_Hand"), 0.0, -0.05, -0.10, -30.0, 180.0, 40.0, 0, 0, 1, 0, 0, 1)
-        SetWeaponAnimationOverride(ped, GetHashKey("Gang1H"))
-        SetCurrentPedWeapon(ped, pistol, true)
-        SetEnableHandcuffs(ped, true)
-    elseif not HasPedGotWeapon(ped, pistol, 0) or not GetSelectedPedWeapon(ped) == pistol
-        print('does not has pistol')
-    --[[
-        shieldActive_two = true
-
-        animDict = "combat@gestures@gang@pistol_1h@beckon"
-        animName = "-90"
-
-        RequestAnimDict(animDict)
-        while not HasAnimDictLoaded(animDict) do
-            Citizen.Wait(100)
-        end
-
-        TaskPlayAnim(ped, animDict, animName, 8.0, -8.0, -1, (2 + 16 + 32), 0.0, 0, 0, 0)
-
-        RequestModel(GetHashKey(prop))
-        while not HasModelLoaded(GetHashKey(prop)) do
-            Citizen.Wait(100)
-        end
-
-        local shield = CreateObject(GetHashKey(prop), pedPos.x, pedPos.y, pedPos.z, 1, 1, 1)
-        shieldEntity = shield
-
-        local others = {
-            xPos = 0.0,
-            yPos = -0.05,
-            zPos = -0.10,
-            xRot = -30.0,
-            yRot = 180.0,
-            zRot = 40.0,
-            p9 = false,
-            useSoftPinning = false,
-            collision = true,
-            isPed = false,
-            vertexIndex = 0,
-            fixedRot = true
-        }
-
-        AttachEntityToEntity(shieldEntity, ped, GetEntityBoneIndexByName(ped, "SKEL_R_Finger32"), others.xPos, others.yPos, others.zPos, others.xRot, others.yRot, others.zRot,others.p9, others.useSoftPinning, others.collision, others.isPed, others.vertexIndex, others.fixedRot)
-
-        --SetWeaponAnimationOverride(ped, GetHashKey("Gang"))
-        --SetCurrentPedWeapon(ped, pistol, true)
-        --SetEnableHandcuffs(ped, true)
-        ]]
-
-        ESX.ShowNotification("Nincs nálad pisztoly!")
-
-    elseif HasPedGotWeapon(ped, pistol, 0) and GetAmmoInPedWeapon(ped, pistol) == < 200
-        AddAmmoToPed(ped, pistol, 550)
-        ESX.ShowNotification("Kaptál ammo-t mivel kevés volt nálad.")
-    end
-end
-
-function DisableShield()
-    local ped = GetPlayerPed(-1)
-    DetachEntity(shieldEntity, true, false)
-    DeleteEntity(shieldEntity)
-    SetEntityAsNoLongerNeeded(shieldEntity)
-    ClearPedTasksImmediately(ped)
-    SetWeaponAnimationOverride(ped, GetHashKey("Default"))
-
-    SetEnableHandcuffs(ped, false)
-    shieldActive = false
-    --shieldActive_two = false
-    shieldEntity = nil
-    animDict = ""
-    animName = ""
-end
 
 RegisterCommand("shield", function(source, args, rawCommand)
     print('command entered')
     TriggerEvent("shield:toggle")
 end, false)
 
+RegisterNetEvent("shield:toggle")
+AddEventHandler("shield:toggle", function()
+    if not wait and shieldActive then
+        DisableShield()
+    elseif not wait and not shieldActive then
+        EnableShield()
+    end
+    print('toggle')
+end)
+
+function EnableShield()
+    wait = true
+    if not wait then
+        local ped = GetPlayerPed(-1)
+        local pedPos = GetEntityCoords(ped, false)
+        local ammo = GetAmmoInPedWeapon(ped, pistol)
+        print('ammo: '..ammo) --need test
+        if HasPedGotWeapon(ped, pistol, 0) or GetSelectedPedWeapon(ped) == pistol then
+            print('has pistol')
+            shieldActive = true
+
+            RequestAnimDict(animDict)
+            while not HasAnimDictLoaded(animDict) do
+                Citizen.Wait(100)
+            end
+
+            TaskPlayAnim(ped, animDict, animName, 8.0, -8.0, -1, (2 + 16 + 32), 0.0, 0, 0, 0)
+
+            RequestModel(GetHashKey(prop))
+            while not HasModelLoaded(GetHashKey(prop)) do
+                Citizen.Wait(100)
+            end
+
+            local shield = CreateObject(GetHashKey(prop), pedPos.x, pedPos.y, pedPos.z, 1, 1, 1)
+            shieldEntity = shield
+
+            AttachEntityToEntity(shieldEntity, ped, GetEntityBoneIndexByName(ped, "IK_L_Hand"), 0.0, -0.05, -0.10, -30.0, 180.0, 40.0, 0, 0, 1, 0, 0, 1)
+            SetWeaponAnimationOverride(ped, GetHashKey("Gang1H"))
+            SetCurrentPedWeapon(ped, pistol, true)
+            SetEnableHandcuffs(ped, true)
+
+        elseif not HasPedGotWeapon(ped, pistol, 0) or not GetSelectedPedWeapon(ped) == pistol then
+            ESX.ShowNotification("Nincs nálad pisztoly!")
+        
+        elseif HasPedGotWeapon(ped, pistol, 0) and (GetAmmoInPedWeapon(ped, pistol) < 200) then
+            AddAmmoToPed(ped, pistol, 550)
+            ESX.ShowNotification("Kaptál ammo-t mivel kevés volt nálad.")
+        end
+    end
+    if shieldActive then
+        wait = false
+    end
+end
+
+function DisableShield()
+    if not wait then
+        wait = true
+
+        while DoesEntityExist(shieldEntity) do
+            DetachEntity(shieldEntity, true, false)
+            DeleteEntity(shieldEntity)
+            SetEntityAsNoLongerNeeded(shieldEntity)
+            SetEntityRoutingBucket(shieldEntity, 355235)
+        end
+        
+        local ped = GetPlayerPed(-1)
+
+        ClearPedTasksImmediately(ped)
+        SetWeaponAnimationOverride(ped, GetHashKey("Default"))
+        SetEnableHandcuffs(ped, false)
+
+        --shieldEntity = nil
+        shieldActive = false
+    end
+    if not shieldActive then
+        wait = false
+    end
+end
+
+
 Citizen.CreateThread(function()
     while true do
-        if shieldActive then
+        Citizen.Wait(100)
+        if not wait and shieldActive then
             local ped = GetPlayerPed(-1)
             if not IsEntityPlayingAnim(ped, animDict, animName, 1) then
                 RequestAnimDict(animDict)
@@ -149,6 +120,10 @@ Citizen.CreateThread(function()
                 end
             
                 TaskPlayAnim(ped, animDict, animName, 8.0, -8.0, -1, (2 + 16 + 32), 0.0, 0, 0, 0)
+            end
+
+            if HasEntityAnimFinished(ped, animDict, animName, 1) then
+                DisableShield()
             end
             SetWeaponAnimationOverride(ped, GetHashKey("Gang1H"))
 
@@ -161,45 +136,7 @@ Citizen.CreateThread(function()
             if not HasAnimDictLoaded(animDict) then
                 Citizen.Wait(0)
             end
-
-            --[[
-            if not IsEntityAttached(shieldEntity) then
-                RequestAnimDict(animDict)
-                
-
-                TaskPlayAnim(ped, animDict, animName, 8.0, -8.0, -1, (2 + 16 + 32), 0.0, 0, 0, 0)
-
-                RequestModel(GetHashKey(prop))
-                while not HasModelLoaded(GetHashKey(prop)) do
-                    Citizen.Wait(100)
-                end
-
-                AttachEntityToEntity(shieldEntity, ped, GetEntityBoneIndexByName(ped, "IK_L_Hand"), 0.0, -0.05, -0.10, -30.0, 180.0, 40.0, 0, 0, 1, 0, 0, 1)
-                SetWeaponAnimationOverride(ped, GetHashKey("Gang1H"))
-            end
-            ]]
-
         end
-        --[[
-        if shieldActive_two then
-            local ped = GetPlayerPed(-1)
-            if not IsEntityPlayingAnim(ped, animDict, animName, 1) then
-                RequestAnimDict(animDict)
-                while not HasAnimDictLoaded(animDict) do
-                    Citizen.Wait(100)
-                end
-            
-                TaskPlayAnim(ped, animDict, animName, 8.0, -8.0, -1, (2 + 16 + 32), 0.0, 0, 0, 0)
-            end
-            --SetWeaponAnimationOverride(ped, GetHashKey("Gang1H"))
-
-            RequestAnimDict(animDict)
-            if not HasAnimDictLoaded(animDict) then
-                Citizen.Wait(0)
-            end
-        end
-        ]]
-        Citizen.Wait(100)
     end
 end)
 
@@ -214,6 +151,8 @@ end)
 
 --notes
 --[[
+
+    -3. használat után nem működik
 
     nem kellett.
     volt collisionje.
