c9f4215e139f267da193187df89d0b973c816529
deep-fivem <60301671+deep-fivem@users.noreply.github.com>
2021-04-10 20:25:23 +0200

    formatting

--- code/server.lua
+++ code/server.lua
@@ -2,9 +2,8 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterUsableItem('shield',
-function(source)
+ESX.RegisterUsableItem('shield', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
-    xPlayer.showNotification('SCRIPT BY DEEP!')
 	TriggerClientEvent("shield:toggle", source)
+	xPlayer.showNotification('SCRIPT BY DEEP!')
 end)
