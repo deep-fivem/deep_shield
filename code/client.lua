18c1f3a1534396bf196ef3c4486e07ba56faf4a6
deep-fivem <60301671+deep-fivem@users.noreply.github.com>
2021-04-12 12:31:48 +0200

    Update client.lua

--- code/client.lua
+++ code/client.lua
@@ -1,13 +1,15 @@
 local shieldActive = false
 local shieldEntity = nil
 local wait = false
+local passed = false
 
 -- ANIM
 local animDict = "combat@gestures@gang@pistol_1h@beckon"
 local animName = "0"
 
 local prop = "prop_ballistic_shield"
-local pistol = GetHashKey("WEAPON_PISTOL")
+local pistol = GetHashKey("weapon_combatpistol")
+local taser = GetHashKey("weapon_stungun")
 
 ESX              = nil
 
@@ -19,7 +21,7 @@ Citizen.CreateThread(function()
 end)
 
 RegisterCommand("ammo", function(source)
-    ESX.ShowNotification("pistol ammo: "..GetAmmoInPedWeapon(GetPlayerPed(source), GetHashKey("WEAPON_PISTOL") ""))
+    ESX.ShowNotification("pistol ammo: "..GetAmmoInPedWeapon(GetPlayerPed(source), GetHashKey("WEAPON_PISTOL")))
 end, false)
 
 RegisterNetEvent("shield:toggle")
@@ -29,24 +31,34 @@ AddEventHandler("shield:toggle", function()
     elseif not wait and not shieldActive then
         EnableShield()
     end
-    print('toggle')
+    --print('toggle')
 end)
 
-RegisterCommand("shield", function(source, args, rawCommand)
-    print('command entered')
-    TriggerEvent("shield:toggle")
-end, false)
+--RegisterCommand("shield", function(source, args, rawCommand)
+--    print('command entered')
+--    TriggerEvent("shield:toggle")
+--end, false)
 
 function EnableShield()
-    wait = true
     if not wait then
+        wait = true
         local ped = GetPlayerPed(-1)
         local pedPos = GetEntityCoords(ped, false)
-        local ammo = GetAmmoInPedWeapon(ped, pistol)
-        print('ammo: '..ammo) --need test
-        if HasPedGotWeapon(ped, pistol, 0) or GetSelectedPedWeapon(ped) == pistol then
-            print('has pistol')
-            shieldActive = true
+        local hasPistol = HasPedGotWeapon(ped, pistol, false)
+        local hasTaser = HasPedGotWeapon(ped, taser, false)
+
+
+        if hasPistol or hasTaser then
+            if hasPistol and not hasTaser then
+                --print('has pistol')
+                SetCurrentPedWeapon(ped, pistol, true)
+            elseif hasTaser and not hasPistol then
+                --print('has taser')
+                SetCurrentPedWeapon(ped, taser, true)
+            elseif hasTaser and hasPistol then
+                --print('has both')
+                SetCurrentPedWeapon(ped, pistol, true)
+            end
 
             RequestAnimDict(animDict)
             while not HasAnimDictLoaded(animDict) do
@@ -65,18 +77,21 @@ function EnableShield()
 
             AttachEntityToEntity(shieldEntity, ped, GetEntityBoneIndexByName(ped, "IK_L_Hand"), 0.0, -0.05, -0.10, -30.0, 180.0, 40.0, 0, 0, 1, 0, 0, 1)
             SetWeaponAnimationOverride(ped, GetHashKey("Gang1H"))
-            SetCurrentPedWeapon(ped, pistol, true)
+            
             SetEnableHandcuffs(ped, true)
 
-        elseif not HasPedGotWeapon(ped, pistol, 0) or not GetSelectedPedWeapon(ped) == pistol then
-            ESX.ShowNotification("Nincs nálad pisztoly!")
-        
-        elseif HasPedGotWeapon(ped, pistol, 0) and (GetAmmoInPedWeapon(ped, pistol) < 200) then
+            passed = false
+            shieldActive = true
+        elseif not hasPistol or not hasTaser then
+            ESX.ShowNotification("Nincs nálad pisztoly vagy sokkoló!")
+            passed = true
+        elseif HasPedGotWeapon(ped, pistol, 0) and GetAmmoInPedWeapon(ped, pistol) < 200 then
             AddAmmoToPed(ped, pistol, 550)
             ESX.ShowNotification("Kaptál ammo-t mivel kevés volt nálad.")
+            passed = true
         end
     end
-    if shieldActive then
+    if shieldActive or passed and wait == true then
         wait = false
     end
 end
@@ -89,9 +104,8 @@ function DisableShield()
             DetachEntity(shieldEntity, true, false)
             DeleteEntity(shieldEntity)
             SetEntityAsNoLongerNeeded(shieldEntity)
-            SetEntityRoutingBucket(shieldEntity, 355235)
         end
-        
+
         local ped = GetPlayerPed(-1)
 
         ClearPedTasksImmediately(ped)
@@ -101,19 +115,17 @@ function DisableShield()
         --shieldEntity = nil
         shieldActive = false
     end
-    if not shieldActive then
+    if not shieldActive and wait == true then
         wait = false
     end
 end
 
-
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(100)
-        if not wait and shieldActive then
+        if shieldActive then
             local ped = GetPlayerPed(-1)
             
-            --make sure anim is playing
             if not IsEntityPlayingAnim(ped, animDict, animName, 1) then
                 RequestAnimDict(animDict)
 
@@ -123,23 +135,28 @@ Citizen.CreateThread(function()
             
                 TaskPlayAnim(ped, animDict, animName, 8.0, -8.0, -1, (2 + 16 + 32), 0.0, 0, 0, 0)
             end
-            --should be useful is the player slips or something like that
-            if HasEntityAnimFinished(ped, animDict, animName, 1) then
-                print('HasEntityAnimFinished')
+
+            if IsPedDeadOrDying(ped) or IsPedDiving(ped) or IsPedFalling(ped) or IsPedGettingIntoAVehicle(ped) or not IsPedOnFoot(ped) or IsPedDucking(ped) or IsPedGettingUp(ped) or IsPedInWrithe(ped) then
                 DisableShield()
+            end
+
+            --TODO
+            local closest_vehicle = ESX.Game.GetClosestVehicle(GetEntityCoords(ped))
+            if closest_vehicle then
+                if GetIsVehicleEngineRunning(closest_vehicle) or IsVehicleEngineStarting(closest_vehicle) then
+                    -- if vehicle moved then disableshield
+                end
             end
             
             SetWeaponAnimationOverride(ped, GetHashKey("Gang1H"))
 
-            -- if selected weapon is changed shield will be disabled
-            if not (GetSelectedPedWeapon(ped) == pistol) then
+            if not (GetSelectedPedWeapon(ped) == pistol or GetSelectedPedWeapon(ped == taser)) then
                 DisableShield()    
             end
         end
     end
 end)
 
---disable on restart
 AddEventHandler('onResourceStop', function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
         return
@@ -147,4 +164,7 @@ AddEventHandler('onResourceStop', function(resourceName)
     print('shield stopped ....')
     DisableShield()
     print('shieldEntity deleted')
-end)  
+end)
+
+--TODO:
+-- optimization, if vehicle moves in front of the player disable shield, going over it for the last time
