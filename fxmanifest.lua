2f37486272ec5dcc52472e26894bc7989f194bc7
deep-fivem <60301671+deep-fivem@users.noreply.github.com>
2021-04-13 22:16:12 +0200

    maybe final update - added config.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,5 +3,7 @@ game 'gta5'
 author 'deep'
 description 'Police Shield item by deep'
 
+client_script "config.lua"
+server_script "config.lua"
 client_script "code/client.lua"
 server_script "code/server.lua"
