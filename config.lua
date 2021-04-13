76414f22218e0c8afff6a1f52f0e2f92b5ba6c48
deep-fivem <60301671+deep-fivem@users.noreply.github.com>
2021-04-13 22:16:36 +0200

    maybe final update - added config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.animDict = "combat@gestures@gang@pistol_1h@beckon"
+Config.animName = "0"
+
+Config.prop = "prop_ballistic_shield"
+
+Config.AmmoTreshold = 180
+Config.Ammo = 250
+
+Config.Weapons = {
+    GetHashKey('WEAPON_PISTOL'),
+    GetHashKey('WEAPON_PISTOL50'),
+    GetHashKey('WEAPON_COMBATPISTOL'),
+    GetHashKey('WEAPON_APPISTOL'),
+    GetHashKey('WEAPON_HEAVYPISTOL'),
+    GetHashKey('WEAPON_STUNGUN'),
+    GetHashKey('WEAPON_VINTAGEPISTOL')
+}
